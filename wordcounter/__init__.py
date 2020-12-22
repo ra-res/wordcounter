9382a31e26acd23af783d7878dd5be7e69d6908f
ra-res <ra-res@users.noreply.github.com>
2020-12-22 21:24:32 +0000

    updated requirements.txt

--- wordcounter/__init__.py
+++ wordcounter/__init__.py
@@ -3,14 +3,21 @@ import os
 from flask import Flask
 from . import counter 
 from paths import paths
+from flask_dropzone import Dropzone
 
 def create_app(test_config=None):
+
     app = Flask(__name__, instance_relative_config=True)
+    dropzone = Dropzone(app)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
 
     app.config['UPLOAD_FOLDER'] = paths['UPLOAD_FOLDER']
+
+    app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+    app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.pdf, .txt'
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
