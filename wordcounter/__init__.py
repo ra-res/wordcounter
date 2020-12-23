f22883f119b5f3b4fb786299e71f1d01a09aa559
ra-res <ra-res@users.noreply.github.com>
2020-12-23 14:52:36 +0000

    deleted dropzone config

--- wordcounter/__init__.py
+++ wordcounter/__init__.py
@@ -14,9 +14,6 @@ def create_app(test_config=None):
 
     app.config['UPLOAD_FOLDER'] = paths['UPLOAD_FOLDER']
 
-    app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
-    app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.pdf, .txt'
-
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
--- wordcounter/counter.py
+++ wordcounter/counter.py
@@ -31,7 +31,6 @@ def delete_files():
 @bp.route('/', methods=['GET', 'POST'])
 def upload_file():
     app = current_app
-    print(request.files)
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
